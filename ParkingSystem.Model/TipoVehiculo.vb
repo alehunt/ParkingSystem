bcafc10848de3a5041910fc3197cb8039ffc9e5e
alehunt <alehunt@gmail.com>
2014-11-01 21:04:19 -0300

    Agregado de Vehiculos al Modelo

---
+++ ParkingSystem.Model/TipoVehiculo.vb
@@ -0,0 +1,42 @@
+﻿Public Class TipoVehiculo
+
+#Region "Private Members"
+    Private mNombre As String
+    Private mValorHora As Decimal
+    Private mValorEstadia As Decimal
+#End Region
+
+#Region "Properties"
+    Public Property ValorEstadia() As Decimal
+        Get
+            Return mValorEstadia
+        End Get
+        Set(ByVal value As Decimal)
+            mValorEstadia = value
+        End Set
+    End Property
+
+
+    Public Property ValorHora() As Decimal
+        Get
+            Return mValorHora
+        End Get
+        Set(ByVal value As Decimal)
+            mValorHora = value
+        End Set
+    End Property
+
+    Public Property Nombre() As String
+        Get
+            Return mNombre
+        End Get
+        Set(ByVal value As String)
+            mNombre = value
+        End Set
+    End Property
+#End Region
+
+
+
+
+End Class
---
+++ ParkingSystem.Model/Vehiculo.vb
@@ -0,0 +1,39 @@
+﻿Public Class Vehiculo
+
+#Region "Private Members"
+    Private mTipo As TipoVehiculo
+    Private mPatente As String
+    Private mAbono As Boolean
+#End Region
+
+#Region "Properties"
+    Public Property Abono() As Boolean
+        Get
+            Return mAbono
+        End Get
+        Set(ByVal value As Boolean)
+            mAbono = value
+        End Set
+    End Property
+
+    Public Property Patente() As String
+        Get
+            Return mPatente
+        End Get
+        Set(ByVal value As String)
+            mPatente = value
+        End Set
+    End Property
+
+
+    Public Property Tipo() As TipoVehiculo
+        Get
+            Return mTipo
+        End Get
+        Set(ByVal value As TipoVehiculo)
+            mTipo = value
+        End Set
+    End Property
+#End Region
+
+End Class
